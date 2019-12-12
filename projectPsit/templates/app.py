1500144b18eb8ce25d41000a44720a44a961d5aa
it62070188 <56476853+it62070188@users.noreply.github.com>
2019-12-12 10:01:29 +0700

    apppython

---
+++ projectPsit/templates/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import random
+app = Flask(__name__)
+
+bodys = ""
+
+@app.route('/templates/first.html')
+def main():
+    return render_template('first.html')
+
+@app.route('/templates/lunch.html')
+def lunch():
+    return render_template('lunch.html')
+
+@app.route('/templates/breakfast.html')
+def breakfast():
+    return render_template('breakfast.html')
+
+
+@app.route('/templates/dinner.html')
+def dinner():
+    return render_template('dinner.html')
+
+@app.route('/input1', methods=['POST', 'GET'])
+def input1():
+    if request.method == 'POST':
+        w = int(request.form['Weight'])
+        t = int(request.form['Height'])
+        body = w / (t/100)**2
+        lis = ['Beef steak', 'pork steak', 'Salmon Salad', 'padthai', 'spakette', 'eeee', 'eeeeeeee', 'dokkkkk', '555555', 'eieieiei']
+        lis_forfat = ['Salad Greens', 'Lean Protein', 'Whole Grains', '6666666', '8888888', '4444444', '777777777', '1121212112', '444444545', '10101010101']
+        if body <= 20:
+            bodys = lis[random.randint(0, 9)]
+        else:
+            bodys = lis1[random.randint(0, 9)]
+        return render_template('lunch.html', body_ssa=bodys)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
